1605b3c0996c239dde3a39eaefa8ec2fc034ce3d
david <davidzakharov0@gmail.com>
2023-04-14 22:29:13 +0300

    commit1

---
+++ main.py
@@ -0,0 +1,14 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+TOKEN = 'vk1.a.gV418o9HEmTOQI2gPvO04uTQ_fzNuRTeH6ruGpoLL6ZtfSrf_QsYWEwGdtGEo2eZLdu7lkkdw8T-bg3gV7Lr8eR4I6H6Qmk9wDbN3vI8dRgR1qLUa3kLXoT3MjLddbZ-CoDmNmLFt1VNFvSscfHtwzPfkkiYyJN-Nq1GUi2abNf_jLTb2dj065ckqDJBxHswivvucSBLXFGLODaT3WaxLA'
+
+vk = vk_api.VkApi(token=TOKEN)
+longpoll = VkLongPoll(vk)
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            print(vars(event))
+
