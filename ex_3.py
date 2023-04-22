98e1cc1a3e0515a561e7084d70e132101c1249fb
MasterHard228 <118759082+MasterHard228@users.noreply.github.com>
2023-04-22 12:05:36 +0300

    commit22

--- ex_3.py
+++ ex_3.py
@@ -1,32 +1,40 @@
 import vk_api
-import random
-import time
+from vk_api.longpoll import VkLongPoll, VkEventType
+import datetime
 
-# функция для генерации рандомной строки
-def generate_random_string(length):
-    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
 
-# функция для отправки сообщения в чат
-def send_message(chat_id, message):
-    vk.method("messages.send", {"peer_id": chat_id, "message": message})
+def get_weekday(date):
+    weekday_dict = {
+        0: 'понедельник',
+        1: 'вторник',
+        2: 'среду',
+        3: 'четверг',
+        4: 'пятницу',
+        5: 'субботу',
+        6: 'воскресенье'
+    }
+    weekday_num = date.weekday()
+    return weekday_dict[weekday_num]
 
-# авторизация в VK API
-vk_session = vk_api.VkApi(token="vk1.a.gV418o9HEmTOQI2gPvO04uTQ_fzNuRTeH6ruGpoLL6ZtfSrf_QsYWEwGdtGEo2eZLdu7lkkdw8T-bg3gV7Lr8eR4I6H6Qmk9wDbN3vI8dRgR1qLUa3kLXoT3MjLddbZ-CoDmNmLFt1VNFvSscfHtwzPfkkiYyJN-Nq1GUi2abNf_jLTb2dj065ckqDJBxHswivvucSBLXFGLODaT3WaxLA")
 
-# создание объекта VK API
-vk = vk_session.get_api()
-
-# получение ID чата
-chat_id = int(input("Введите ID чата: "))
-
-# отправка сообщений в чат до тех пор, пока не придет команда /stop
-while True:
-    message = generate_random_string(10)
-    send_message(chat_id, message)
-    time.sleep(3) # задержка в 3 секунды
-    messages = vk.method("messages.getConversationsById", {"peer_ids": chat_id, "count": 1})
-    if messages["items"][0]["last_message"]["text"] == "/stop":
-        send_message(chat_id, "Бот остановлен.")
-        break
+vk_session = vk_api.VkApi(
+    token='vk1.a.gV418o9HEmTOQI2gPvO04uTQ_fzNuRTeH6ruGpoLL6ZtfSrf_QsYWEwGdtGEo2eZLdu7lkkdw8T-bg3gV7Lr8eR4I6H6Qmk9wDbN3vI8dRgR1qLUa3kLXoT3MjLddbZ-CoDmNmLFt1VNFvSscfHtwzPfkkiYyJN-Nq1GUi2abNf_jLTb2dj065ckqDJBxHswivvucSBLXFGLODaT3WaxLA')
+longpoll = VkLongPoll(vk_session)
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        message = event.text.lower()
+        if 'день недели' in message:
+            vk = vk_session.get_api()
+            vk.messages.send(chat_id=event.chat_id,
+                             message='Введите дату в формате YYYY-MM-DD:')
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    date_str = event.text
+                    try:
+                        date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+                        weekday = get_weekday(date)
+                        vk.messages.send(chat_id=event.chat_id,
+                                         message=f'Дата {date_str} была в {weekday}.')
+                    except ValueError:
+                        vk.messages.send(chat_id=event.chat_id,
+                                         message='Некорректный формат даты. Попробуйте еще раз.')
