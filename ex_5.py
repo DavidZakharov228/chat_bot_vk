0eda9ac5e3889c75c5262c97136a364db7d9c910
MasterHard228 <118759082+MasterHard228@users.noreply.github.com>
2023-04-23 14:09:19 +0300

    commit14

---
+++ ex_5.py
@@ -0,0 +1,27 @@
+import asyncio
+import time
+
+async def do_task(name, task_num, prep_time, def_time):
+    print(f"{name} started the {task_num} task.")
+    await asyncio.sleep(prep_time)
+    print(f"{name} moved on to the defense of the {task_num} task.")
+    await asyncio.sleep(def_time)
+    print(f"{name} completed the {task_num} task.")
+
+async def do_tasks(name, task_num, prep_time, def_time, rest_time):
+    await do_task(name, task_num, prep_time, def_time)
+    print(f"{name} is resting.")
+    await asyncio.sleep(rest_time)
+
+async def interviews(*applicants):
+    tasks = []
+    for name, task1_prep, task1_def, task2_prep, task2_def in applicants:
+        tasks.append(do_tasks(name, 1, task1_prep, task1_def, task2_prep))
+        await asyncio.gather(*tasks)
+        tasks.append(do_tasks(name, 2, task2_prep, task2_def, 0))
+        await asyncio.gather(*tasks)
+
+data = [('Ivan', 5, 2, 7, 2), ('John', 3, 4, 5, 1), ('Sophia', 4, 2, 5, 1)]
+t0 = time.time()
+asyncio.run(interviews(*data))
+print(time.time() - t0)
